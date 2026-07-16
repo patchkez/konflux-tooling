605266770810f8ba8aae24cdaebc0ac603d47208
Viliam Krizan <vkrizan@redhat.com>
2026-07-16 11:49:43 +0200

    refactor: do repo enable once

--- gen-rpms-lock-yaml.sh
+++ gen-rpms-lock-yaml.sh
@@ -33,8 +33,8 @@ fi
 
 if [ $SUB ]; then
   echo '=== Enable repositories ==='
-  subscription-manager repos --enable=rhel-${UBI_VERSION}-for-x86_64-appstream-source-rpms
-  subscription-manager repos --enable=rhel-${UBI_VERSION}-for-x86_64-baseos-source-rpms
+  subscription-manager repos --enable=rhel-${UBI_VERSION}-for-x86_64-appstream-source-rpms \
+                             --enable=rhel-${UBI_VERSION}-for-x86_64-baseos-source-rpms
 fi
 
 echo "=== Looping through repo files ==="
